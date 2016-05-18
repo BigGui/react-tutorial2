8170d97374637ee1dd6570947f0c34497077b49e
Gbelleuvre <Gbelleuvre>
2016-05-18 12:43:29 +0200

    ajout suppression

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,3 @@
-var TodoList = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<TodoHeader />
-				<section id="main">
-		          <input id="toggle-all" type="checkbox" />
-		          <label htmlFor="toggle-all">Mark all as complete</label>
-		          <ul id="todo-list">
-		            <li className="completed">
-		              <div className="view">
-		                <input className="toggle" type="checkbox" checked />
-		                <label>Create a TodoMVC template</label>
-		                <button className="destroy"></button>
-		              </div>
-		              <input className="edit" value="Create a TodoMVC template" />
-		            </li>
-		            <li>
-		              <div className="view">
-		                <input className="toggle" type="checkbox" />
-		                <label>Rule the we</label>
-		                <button className="destroy"></button>
-		              </div>
-		            </li>
-		          </ul>
-		        </section>
-				<TodoFooter />
-	        </div>
-		);
-	}
-});
 
 var TodoHeader = React.createClass({
 	render: function() {
@@ -40,6 +9,7 @@ var TodoHeader = React.createClass({
 		);
 	}
 });
+
 
 var TodoFooter = React.createClass({
 	render: function() {
@@ -62,5 +32,74 @@ var TodoFooter = React.createClass({
 		);
 	}
 });
+
+
+var TodoItem = React.createClass({
+	delete: function() {
+		this.props.delete(this.props.itemId);
+	},
+
+	render: function() {
+
+		var classStr = '';
+
+		if (this.props.todo.completed) classStr += 'completed';
+
+		return (
+            <li className={classStr}>
+              <div className="view">
+                <input className="toggle" type="checkbox" checked />
+                <label>{this.props.todo.name}</label>
+                <button onClick={this.delete} className="destroy"></button>
+              </div>
+              <input className="edit" value="Create a TodoMVC template" />
+            </li>
+		);
+	}
+});
+
+
+var TodoList = React.createClass({
+
+	deleteItem: function(itemId) {
+		this.state.todos.splice(itemId, 1);
+		this.setState({todos: this.state.todos});
+	},
+
+	getInitialState: function() {
+
+		return { todos: [
+			{ name: "Faire la vaisselle", completed: true },
+			{ name: "Faire le ménage", completed: false }
+		]};
+
+	},
+
+	render: function() {
+
+		var self = this;
+
+		var rows = this.state.todos.map(function(todo, i) {
+			return (
+				<TodoItem todo={todo} itemId={i} delete={self.deleteItem} />
+			);
+		});
+
+		return (
+			<div>
+				<TodoHeader />
+				<section id="main">
+		          <input id="toggle-all" type="checkbox" />
+		          <label htmlFor="toggle-all">Mark all as complete</label>
+		          <ul id="todo-list">
+		            {rows}
+		          </ul>
+		        </section>
+				<TodoFooter />
+	        </div>
+		);
+	}
+});
+
 
 ReactDOM.render(<TodoList />, document.getElementById("todoapp"));
